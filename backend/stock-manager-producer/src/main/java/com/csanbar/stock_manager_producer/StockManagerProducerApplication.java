f6c7cc00ab45eb00d179d3bb76d0373a32bcb5da
csanbardev <csanbar97@gmail.com>
2024-06-06 18:08:27 +0200

    feat: :tada: init stock-manager-producer

---
+++ backend/stock-manager-producer/src/main/java/com/csanbar/stock_manager_producer/StockManagerProducerApplication.java
@@ -0,0 +1,13 @@
+package com.csanbar.stock_manager_producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockManagerProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockManagerProducerApplication.class, args);
+	}
+
+}
---
+++ backend/stock-manager-producer/src/test/java/com/csanbar/stock_manager_producer/StockManagerProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csanbar.stock_manager_producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockManagerProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
