634386784d6be01b1d0db1f5d9cc913ee5765abc
csanbardev <csanbar97@gmail.com>
2024-06-18 17:02:12 +0200

    feat: :sparkles: create & implement Product created event for Kafka

--- backend/stock-manager-producer/src/main/java/com/csanbar/stock_manager_producer/services/ProductService.java
+++ backend/stock-manager-producer/src/main/java/com/csanbar/stock_manager_producer/services/ProductService.java
@@ -2,6 +2,7 @@ package com.csanbar.stock_manager_producer.services;
 
 import com.csanbar.stock_manager_producer.models.Product;
 import com.csanbar.stock_manager_producer.repositories.ProductRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,16 +10,20 @@ import java.util.List;
 @Service
 public class ProductService {
     private final ProductRepository repository;
+    private final ProductEventsService productEventsService;
 
-    public ProductService(ProductRepository repository) {
+    public ProductService(ProductRepository repository, ProductEventsService productEventsService) {
         this.repository = repository;
+        this.productEventsService = productEventsService;
     }
+
 
     public List<Product> getAll() {
         return repository.getAllProducts();
     }
 
     public long createProduct(Product product) {
+        this.productEventsService.publish(product);
         return repository.createProduct(product);
     }
 
