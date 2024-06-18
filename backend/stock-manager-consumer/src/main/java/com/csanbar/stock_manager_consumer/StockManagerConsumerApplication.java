37614b279fa00ae2ec97498c0ea1ff2030ed1a36
csanbardev <csanbar97@gmail.com>
2024-06-18 17:24:44 +0200

    feat: :tada: init stock-manager-consumer

---
+++ backend/stock-manager-consumer/src/main/java/com/csanbar/stock_manager_consumer/StockManagerConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.csanbar.stock_manager_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockManagerConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockManagerConsumerApplication.class, args);
+	}
+
+}
