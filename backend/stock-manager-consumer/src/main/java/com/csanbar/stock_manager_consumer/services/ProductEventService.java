5069c47227ad2c299412d01187b519f1b8e86354
csanbardev <csanbar97@gmail.com>
2024-06-20 20:11:43 +0200

    feat: :sparkles: implement Kafka listener & update mongodb database logic

---
+++ backend/stock-manager-consumer/src/main/java/com/csanbar/stock_manager_consumer/services/ProductEventService.java
@@ -0,0 +1,44 @@
+package com.csanbar.stock_manager_consumer.services;
+
+import com.csanbar.stock_manager_consumer.events.Event;
+import com.csanbar.stock_manager_consumer.events.ProductCreatedEvent;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class ProductEventService {
+
+
+    private final ObjectMapper objectMapper;
+
+    private final ProductService productService;
+
+    public ProductEventService(ObjectMapper objectMapper, ProductService productService) {
+        this.objectMapper = objectMapper;
+        this.productService = productService;
+    }
+
+    @KafkaListener(
+            topics = "${topic.product.name:products}",
+            containerFactory = "kafkaListenerContainerFactory",
+            groupId = "grupo1"
+    )
+    public void consumer(String event) {
+        try {
+            Event<?> productEvent = objectMapper.readValue(event, Event.class);
+            if (productEvent instanceof ProductCreatedEvent productCreatedEvent) {
+                productService.createProduct(productCreatedEvent.getData());
+                log.info("Received Product ... with id={}, data={}",
+                        productCreatedEvent.getId(),
+                        productCreatedEvent.getData().toString());
+            } else {
+                log.warn("Unknown event type: {}", productEvent.getType());
+            }
+        } catch (Exception e) {
+            log.error("Error processing event: " + event, e);
+        }
+    }
+}
