2445531637d912f3727385a8fcb7ef5858ae6b28
csanbardev <csanbar97@gmail.com>
2024-07-02 16:23:57 +0200

    feat: :sparkles: implement Kafka Purchase creation event

--- backend/stock-manager-producer/src/main/java/com/csanbar/stock_manager_producer/services/PurchaseService.java
+++ backend/stock-manager-producer/src/main/java/com/csanbar/stock_manager_producer/services/PurchaseService.java
@@ -4,16 +4,19 @@ import com.csanbar.stock_manager_producer.controllers.PurchaseController;
 import com.csanbar.stock_manager_producer.models.Purchase;
 import com.csanbar.stock_manager_producer.repositories.ProductPurchaseRepository;
 import com.csanbar.stock_manager_producer.repositories.PurchaseRepository;
+import com.csanbar.stock_manager_producer.services.events.PurchaseEventsService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PurchaseService {
     private final PurchaseRepository purchaseRepository;
     private final ProductPurchaseRepository productPurchaseRepository;
+    private final PurchaseEventsService purchaseEventsService;
 
-    public PurchaseService(PurchaseRepository purchaseRepository, ProductPurchaseRepository productPurchaseRepository) {
+    public PurchaseService(PurchaseRepository purchaseRepository, ProductPurchaseRepository productPurchaseRepository, PurchaseEventsService purchaseEventsService) {
         this.purchaseRepository = purchaseRepository;
         this.productPurchaseRepository = productPurchaseRepository;
+        this.purchaseEventsService = purchaseEventsService;
     }
 
     public long createPurchase(Purchase purchase) {
@@ -21,7 +24,9 @@ public class PurchaseService {
             long created = purchaseRepository.createPurchase(purchase);
 
             productPurchaseRepository.assignPurchases(purchase.productList, created);
-            purchase.pur_id = purchase.pur_id;
+            purchase.pur_id = created;
+
+            purchaseEventsService.publish(purchase);
 
             return created;
         } catch (Exception e) {
---
+++ backend/stock-manager-producer/src/main/java/com/csanbar/stock_manager_producer/services/events/PurchaseEventsService.java
@@ -0,0 +1,32 @@
+package com.csanbar.stock_manager_producer.services.events;
+
+import com.csanbar.stock_manager_producer.events.Event;
+import com.csanbar.stock_manager_producer.events.EventType;
+import com.csanbar.stock_manager_producer.events.purchase.PurchaseCreatedEvent;
+import com.csanbar.stock_manager_producer.models.Purchase;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Service
+public class PurchaseEventsService {
+    private KafkaTemplate<String, Event<?>> producer;
+
+
+    @Value("${topic.purchase.name:purchases}")
+    private String topicProduct;
+
+    public void publish(Purchase purchase){
+        PurchaseCreatedEvent created = new PurchaseCreatedEvent();
+
+        created.setData(purchase);
+        created.setId(UUID.randomUUID().toString());
+        created.setType(EventType.PURCHASE_CREATED);
+        created.setDate(new Date());
+
+        this.producer.send(topicProduct, created);
+    }
+}
