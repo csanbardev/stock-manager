ac4a254246cf7c4bae8190987b4d4efcf599229f
csanbardev <csanbar97@gmail.com>
2024-07-10 11:12:13 +0200

    feat: :tada: init stock manager gateway

---
+++ backend/stock-manager-gateway/src/main/java/com/csanbar/stock_manager_gateway/StockManagerGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.csanbar.stock_manager_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockManagerGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockManagerGatewayApplication.class, args);
+	}
+
+}
